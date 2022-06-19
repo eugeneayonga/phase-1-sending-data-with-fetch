6625434d75c18fa9b2b004062242d994b353bb47
Eugene <eugeneayonga@gmail.com>
2022-06-19 10:11:58 +0300

    commit tree 1f43c4fe5b800e099aab193b5e472d428b1a97bd

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData (userName, userEmail) {
+    
+    const user = {
+        name: userName,
+        email: userEmail
+    }
+
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(user),
+    };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.body.innerHTML += `<p>${data.id}</p>`;
+    })
+    .catch(error => {
+        console.log(error);
+        document.body.innerHTML += `<p>${error.message}</p>`;
+    });
+}
